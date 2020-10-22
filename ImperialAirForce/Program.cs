3bf3f8b4ede24b4de674efe90767475f1de5fce4
David Kaufmann <david@kaufmann.dev>
2020-10-22 13:39:57 +0200

    Initial commit

---
+++ ImperialAirForce/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ImperialAirForce {
+    class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
