41d31df57c862d9c04982f103a91dd5a0c5eecf0
David Kaufmann <david@kaufmann.dev>
2020-11-02 20:55:53 +0100

    Point erstellt

---
+++ ImperialAirForce/Point.cs
@@ -0,0 +1,28 @@
+﻿namespace ImperialAirForce
+{
+    public class Point
+    {
+        protected int _x;
+        protected int _y;
+        protected int _z;
+
+        public int X {
+            get =>_x;
+            set =>_x = value;
+        }
+        public int Y {
+            get =>_y;
+            set =>_y = value;
+        }
+        public int Z {
+            get =>_z;
+            set =>_z = value;
+        }
+
+        public Point(int x, int y, int z) {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+    }
+}
