92593c9f2468cf1e8473e059d0b96e5a01af1cf3
David Kaufmann <david@kaufmann.dev>
2020-11-03 13:30:58 +0100

    Updated Unit.cs

--- ImperialAirForce/Unit.cs
+++ ImperialAirForce/Unit.cs
@@ -2,6 +2,27 @@
 {
     public class Unit
     {
-        
+        protected int _structure;
+        protected int _speed;
+        protected int _minSpeed;
+        protected int _maxSpeed;
+        protected int _handling;
+        protected int _altitude;
+        protected int _maxAltitude;
+        protected int _maxThrottle;
+        protected int _maxManeuver;
+
+
+        public Unit(int structure, int speed, int maxThrottle, int minSpeed, int maxSpeed, int maxManeuver, int handling, int maxAltitude, int altitude) {
+            _handling = handling;
+            _structure = structure;
+            _speed = speed;
+            _minSpeed = minSpeed;
+            _maxSpeed = maxSpeed;
+            _maxThrottle = maxThrottle;
+            _maxManeuver = maxManeuver;
+            _maxAltitude = maxAltitude;
+            _altitude = altitude;
+        }
     }
 }
