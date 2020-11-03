7d0f9d887ea8d8df25ffbe7bdbc823f6fca57242
David Kaufmann <david@kaufmann.dev>
2020-11-03 20:30:57 +0100

    Updated Imp.cs and Ork.cs

--- ImperialAirForce/Imp.cs
+++ ImperialAirForce/Imp.cs
@@ -1,7 +1,7 @@
 ﻿namespace ImperialAirForce
 {
-    public class Imp
+    public class Imp : Unit
     {
-        
+        public Imp(int structure, int speed,  int handling, int maxThrottle, int minSpeed, int maxSpeed, int maxManeuver, int maxAltitude, int altitude) : base(structure, speed, maxThrottle, minSpeed, maxSpeed, maxManeuver, handling, maxAltitude, altitude) {}
     }
 }
--- ImperialAirForce/Ork.cs
+++ ImperialAirForce/Ork.cs
@@ -1,7 +1,7 @@
 ﻿namespace ImperialAirForce
 {
-    public class Ork
+    public class Ork : Unit
     {
-        
+        public Ork(int structure, int speed, int handling, int maxThrottle, int minSpeed, int maxSpeed, int maxManeuver, int maxAltitude, int altitude) : base(structure, speed, maxThrottle, minSpeed, maxSpeed, maxManeuver, handling, maxAltitude, altitude) {}
     }
 }
